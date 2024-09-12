507b348fe4ebab0a21f7dc411c88d8674a29f278
Max Andreev <maxick20@gmail.com>
2024-09-12 23:03:57 +0400

    chore: setup prettier

--- contracts/EventManager.sol
+++ contracts/EventManager.sol
@@ -5,10 +5,6 @@ import {UniqueV2TokenMinter} from "unique-contracts/UniqueV2TokenMinter.sol";
 import {UniqueV2CollectionMinter} from "unique-contracts/UniqueV2CollectionMinter.sol";
 
 contract EventManager is UniqueV2CollectionMinter, UniqueV2TokenMinter {
-
-  ///@dev all token properties will be mutable for collection admin
-  constructor() payable UniqueV2CollectionMinter(true, false, true) {}
-
-  receive() external payable {}
-
+    ///@dev all token properties will be mutable for collection admin
+    constructor() payable UniqueV2CollectionMinter(true, false, true) {}
 }

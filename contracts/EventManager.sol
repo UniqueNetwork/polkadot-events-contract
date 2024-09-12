81239444d3c308810865b550a7d7f0d2b5883579
Max Andreev <maxick20@gmail.com>
2024-09-12 23:02:51 +0400

    feat: add unique-contracts deps

---
+++ contracts/EventManager.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {UniqueV2TokenMinter} from "unique-contracts/UniqueV2TokenMinter.sol";
+import {UniqueV2CollectionMinter} from "unique-contracts/UniqueV2CollectionMinter.sol";
+
+contract EventManager is UniqueV2CollectionMinter, UniqueV2TokenMinter {
+
+  ///@dev all token properties will be mutable for collection admin
+  constructor() payable UniqueV2CollectionMinter(true, false, true) {}
+
+  receive() external payable {}
+
+}
